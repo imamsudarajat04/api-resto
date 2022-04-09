2ae6a4614f2faaec18fa221d3a1ab56cc857d9c4
imamsudarajat04 <imamsudrajat707@gmail.com>
2022-04-09 21:10:51 +0700

    Update Table Users

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,8 +19,10 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
+        'no_hp',
         'email',
         'password',
+        'address',
     ];
 
     /**
